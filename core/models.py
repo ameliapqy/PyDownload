ec1bf2f5adf3c086d32894c6ddfd013fe78582a5
pqy <pqy@seas.upenn.edu>
2020-05-02 13:47:40 -0400

    add magic method

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Video(models.Model):
+    title = models.CharField(max_length=2800, null=True)
+    url = models.CharField(max_length=2800, null=True)
+    thumbnail = models.CharField(max_length=2800, null=True)
+    def __str__(self):
+        return self.title
+
+    def __init__(self, title, url, thumbnail):
+        super().__init__()
+        self.title = title
+        self.url = url
+        self.thumbnail = thumbnail
--- core/views.py
+++ core/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, redirect
 from django.conf import settings
 import json, requests
-# Create your views here.
+from pytube import YouTube
+import os, shutil
+from core.models import Video
+#return the main search page
 def search(request):
     return render(request, "search.html", {"query":""})
 
+#display the result by fetching data from youtubeAPI
 def result(request):
     if "query" not in request.POST:
         print("no query no action")
@@ -21,27 +25,37 @@ def result(request):
         'type': 'video'
     }
     all_data = []
+    videos = []
     r = requests.get(search_url, params=search_params)
     results = r.json()['items']
     for result in results: 
-        curr_data = {
-            'title' : result['snippet']['title'],
-            'url' : 'https://www.youtube.com/watch?v=' + result["id"]["videoId"],
-            'thumbnail' : result['snippet']['thumbnails']['medium']['url']
-        }
-        all_data.append(curr_data)
+        # curr_data = {
+        #     'title' : result['snippet']['title'],
+        #     'url' : 'https://www.youtube.com/watch?v=' + result["id"]["videoId"],
+        #     'thumbnail' : result['snippet']['thumbnails']['medium']['url']
+        # }
+        title  = result['snippet']['title']
+        url = 'https://www.youtube.com/watch?v=' + result["id"]["videoId"]
+        thumbnail = result['snippet']['thumbnails']['medium']['url']
+        video = Video.objects.create(title=title, url = url, thumbnail = thumbnail)
+        videos.append(video)
     if "download" in request.POST:
         print("detected download")
         url = request.POST["download"]
         download(url)
         print("download finished")
         # return render(request, "result.html", {"query":"N/A", "data": []})
-    return render(request, "result.html", {"query":query, "data": all_data})
+    return render(request, "result.html", {"query":query, "data": videos})
 
-
-from pytube import YouTube
-
+#helper method to download youtube video
 def download(url):
     yt = YouTube(url)
     video = yt.streams.first()
-    video.download("/Users/qingyuanpeng/Desktop")
+    video.download("downloads")
+
+def empty_folder():
+    for root, dirs, files in os.walk("downloads"):
+        for f in files:
+            os.unlink(os.path.join(root, f))
+        for d in dirs:
+            shutil.rmtree(os.path.join(root, d))
